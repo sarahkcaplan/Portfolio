29ef43ed599d2df07bf27467dda7ad37fc7e5022
Sarah Caplan <caplan.sarah@gmail.com>
2016-11-12 20:24:58 -0700

    Updating image sizing 2nd time

--- gruntfile.js
+++ gruntfile.js
@@ -11,22 +11,22 @@ module.exports = function(grunt) {
           engine: 'im',
           sizes: [{
             name: 'xs',
-            width: '50%',
+            width: '75%',
             suffix: '_x-small',
             quality: 40
           },{
             name: 's',
-            width: '60%',
+            width: '80%',
             suffix: '_small',
             quality: 40
           },{
             name: 'm',
-            width: '80%',
+            width: '95%',
             suffix: '_medium',
             quality: 60
           },{
             name: 'l',
-            width: '95%',
+            width: '100%',
             suffix: '_large',
             quality: 80
       	 }]
